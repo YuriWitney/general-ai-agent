c0c74af15dc9bade39fb9ff838e6dd10ffd1bf7d
Witney Feitoza Barros <yfeitoza@minsait.com>
2026-02-09 13:13:03 -0300

    test: add services test

---
+++ src/modules/agents/__tests__/services.spec.ts
@@ -0,0 +1,39 @@
+import { Mocked, expect, describe, it } from 'vitest'
+import { AgentService } from '../service'
+import { IAgentFactory } from '../../../interfaces/IAgentFactory'
+import { IConfig } from '../../../interfaces/IConfig'
+import { makeBaseMessageStub } from './helpers/test-helper'
+import { IAgentService } from '../interfaces'
+
+const makeAgentFactoryStub = (): Mocked<IAgentFactory> => ({
+  createReactAgent: vi.fn().mockReturnValue({
+    invoke: vi.fn().mockResolvedValue(makeBaseMessageStub('agent invoke test')),
+    streamEvents: vi.fn().mockResolvedValue(makeBaseMessageStub('agent stream events test'))
+  })
+})
+
+const makeFakeConfig = (): IConfig => ({
+  apiKey: 'some_api_key',
+  modelName: 'some_model',
+  temperature: 0,
+  threadId: '123'
+})
+
+const makeSut = (): IAgentService => {
+  const agentFactoryStub = makeAgentFactoryStub()
+  const configStub = makeFakeConfig()
+  return new AgentService(agentFactoryStub, configStub)
+}
+
+describe(`${AgentService.name} tests`, () => {
+  describe(`When ${AgentService.prototype.invoke.name} method is called`, () => {
+    it(`Should call ${AgentService.prototype.invoke.name} with correct params`, async () => {
+      const sut = makeSut()
+      const fakeInput = makeBaseMessageStub('some_input')
+
+      const result = await sut.invoke(fakeInput)
+
+      expect(result).toEqual(makeBaseMessageStub('agent invoke test').messages[0])
+    })
+  })
+})
